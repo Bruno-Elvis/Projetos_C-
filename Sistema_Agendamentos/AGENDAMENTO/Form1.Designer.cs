53d11ac0697573f77d463851233574e0e304b7cd
Bruno Elvis <50249992+Bruno-Elvis@users.noreply.github.com>
2021-06-05 14:59:56 -0300

    Add files via upload

---
+++ Sistema_Agendamentos/AGENDAMENTO/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace AGENDAMENTO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.listaPaciente = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.listaPaciente)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(13, 12);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // listaPaciente
+            // 
+            this.listaPaciente.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listaPaciente.BackgroundColor = System.Drawing.Color.Bisque;
+            this.listaPaciente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listaPaciente.Location = new System.Drawing.Point(13, 41);
+            this.listaPaciente.Name = "listaPaciente";
+            this.listaPaciente.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.listaPaciente.Size = new System.Drawing.Size(582, 352);
+            this.listaPaciente.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(607, 405);
+            this.Controls.Add(this.listaPaciente);
+            this.Controls.Add(this.btnAbrir);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AGENDAMENTO";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.listaPaciente)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.DataGridView listaPaciente;
+    }
+}
+
---
+++ Sistema_Agendamentos/AGENDAMENTO/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AGENDAMENTO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listaPaciente.ColumnCount = 15;
+            listaPaciente.Columns[0].Name = "Nome";
+            listaPaciente.Columns[1].Name = "CPF";
+            listaPaciente.Columns[2].Name = "Identidade";
+            listaPaciente.Columns[3].Name = "Data Nascimento";
+            listaPaciente.Columns[4].Name = "Rua";
+            listaPaciente.Columns[5].Name = "Bairro";
+            listaPaciente.Columns[6].Name = "Cidade";
+            listaPaciente.Columns[7].Name = "País";
+            listaPaciente.Columns[8].Name = "Alergias";
+            listaPaciente.Columns[9].Name = "Tipo Saguíneos";
+            listaPaciente.Columns[10].Name = "Peso";
+            listaPaciente.Columns[11].Name = "Altura";
+            listaPaciente.Columns[12].Name = "IMC";
+            listaPaciente.Columns[13].Name = "Data Cadastro";
+            listaPaciente.Columns[14].Name = "Última consulta";
+            listaPaciente.DefaultCellStyle.Font = new Font("Tahoma", 6);
+            listaPaciente.DefaultCellStyle.ForeColor = Color.Blue;
+            listaPaciente.DefaultCellStyle.BackColor = Color.Beige;
+            listaPaciente.DefaultCellStyle.SelectionForeColor = Color.Black;
+            listaPaciente.DefaultCellStyle.SelectionBackColor = Color.AntiqueWhite;
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listaPaciente.Rows.Clear();
+                string linha = "";
+                int contador = 0;
+
+                OpenFileDialog abrir = new OpenFileDialog();
+                abrir.Filter = "Texto|*.txt";
+
+                if (abrir.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    StreamReader arquivo = new StreamReader(abrir.FileName);
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        string[] valores = linha.Split('|');
+                        listaPaciente.Rows.Add(valores);
+                        contador++;
+                    }
+                    
+                    if (contador == 0)
+                    {
+                        MessageBox.Show("ARQUIVO DE TEXTO VAZIO!");
+                    }
+                }
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("ERRO NO SISTEMA: " + erro.Message);
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
